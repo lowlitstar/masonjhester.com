64620233868e951f3506347adf2af56237861718
sahdudeone <93887421+sahdudeone@users.noreply.github.com>
2023-04-11 13:57:21 -0700

    update 0.1.1

--- mod_scripts/main.js
+++ mod_scripts/main.js
@@ -1,17 +1,2 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
-
-const subTitle2 = document.querySelector('.sub__title2');
-const intro = document.querySelector('.intro');
-
-document.addEventListener('mousemove', (event) => {
-  if (window.innerWidth > 768) {
-    const x = event.clientX;
-    const y = event.clientY;
-    subTitle2.style.left = `${x}px`;
-    subTitle2.style.top = `${y}px`;
-  } else {
-    subTitle2.style.left = '';
-    subTitle2.style.top = '';
-  }
-});
