8a8b534df0085943797c75abca3fb65561f89c97
sahdudeone <93887421+sahdudeone@users.noreply.github.com>
2023-04-11 17:25:23 -0700

    update 0.1.3.6

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        chaos: resolve(__dirname, 'p/chaos.html'),
+        chaos: resolve(__dirname, 'chaos.html'),
       },
     },
   },
