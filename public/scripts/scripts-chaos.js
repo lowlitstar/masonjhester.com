2acb2a8cba1afd7d4a74a8d587067ca6f0012fe2
sahdudeone <93887421+sahdudeone@users.noreply.github.com>
2023-08-06 02:40:24 -0700

    0.4.7 featured lightbox

--- public/scripts/scripts-chaos.js
+++ public/scripts/scripts-chaos.js
@@ -1,7 +1,18 @@
-const lineSpans = document.querySelectorAll('.line span');
+document.addEventListener('DOMContentLoaded', (event) => {
+  const lineSpans = document.querySelectorAll('.line span');
 
-lineSpans.forEach(span => {
-  span.addEventListener('click', () => {
-    span.classList.toggle('glow');
+  lineSpans.forEach(span => {
+    span.addEventListener('click', () => {
+      span.classList.toggle('glow');
+    });
+  });
+
+  document.getElementById("lightboxedImage").addEventListener("click", function() {
+    document.getElementById("lightbox").classList.remove("hidden");
+    document.getElementById("lightboxImage").src = this.src;
+  });
+
+  document.getElementById("lightbox").addEventListener("click", function() {
+    this.classList.add("hidden");
   });
 });
